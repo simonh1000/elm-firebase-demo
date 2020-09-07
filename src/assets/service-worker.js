4273ddf83b78cd35b3a8035db55d44c3bef1fc15
Simon Hampton <hotbelgo@gmail.com>
2020-09-07 07:49:17 +0200

    compiles :-)

--- src/assets/service-worker.js
+++ src/assets/service-worker.js
@@ -1,21 +1,14 @@
-// unneeded as this is added (albeit version 4.1.3) by webpack when properly configured
-// importScripts(
-//     "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
-// );
+import { precacheAndRoute } from "workbox-precaching";
 
-// Doesn't work - error: "can't import module"
-// import { precacheAndRoute } from "workbox-precaching";
-if (workbox) {
-    console.log(`Setting up cache 🎉`);
-    // The precache manifest lists the names of the files that were processed by webpack and that end up in your dist folder.
-    workbox.precaching.precacheAndRoute(self.__precacheManifest);
+console.log(`Setting up cache 🎉`);
 
-    addEventListener("message", (event) => {
-        // console.log("[service-worker.js] message", event.data);
-        if (event.data && event.data.type === "SKIP_WAITING") {
-            skipWaiting();
-        }
-    });
-} else {
-    console.log(`Hmm! Workbox didn't load 😬`);
-}
+// The precache manifest lists the names of the files that were processed by webpack and that end up in your dist folder.
+precacheAndRoute(self.__WB_MANIFEST);
+
+// enables a new SW to replace an existing one when a user clicks OK to do so
+addEventListener("message", (event) => {
+    // console.log("[service-worker.js] message", event.data);
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        skipWaiting();
+    }
+});
